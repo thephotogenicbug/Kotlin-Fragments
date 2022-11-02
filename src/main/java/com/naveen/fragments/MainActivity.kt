d0d1770492b13f20a91a85d99404cd5fa4b4410d
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-02 12:43:20 +0530

    Fragments

--- src/main/java/com/naveen/fragments/MainActivity.kt
+++ src/main/java/com/naveen/fragments/MainActivity.kt
@@ -2,10 +2,41 @@ package com.naveen.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var replace : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replace = findViewById(R.id.buttonReplace)
+
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val fragmentTransaction : FragmentTransaction = fragmentManager.beginTransaction()
+
+        val firstFragment = FirstFragment()
+
+        fragmentTransaction.add(R.id.frame, firstFragment)
+
+        fragmentTransaction.commit()
+
+        replace.setOnClickListener {
+
+            val secondFragmentManager : FragmentManager = supportFragmentManager
+            val secondFragmentTransaction : FragmentTransaction = secondFragmentManager.beginTransaction()
+
+            val secondFragment = SecondFragment()
+
+            secondFragmentTransaction.replace(R.id.frame, secondFragment)
+
+            secondFragmentTransaction.commit()
+        }
+
+
     }
 }
